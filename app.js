a275e7f946b9e93189755d3e2f6c379a56042081
devnido <luis.nido@hotmail.com>
2016-09-25 19:12:42 -0300

    correciones

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require("express"),
     bodyParser  = require("body-parser"),
     mongoose = require('mongoose');
 
+var models     = require('./models/tvshows')(app, mongoose);
+var TVShowCtrl = require('./controllers/tvshows');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -14,6 +17,30 @@ router.get('/', function(req, res) {
 });
 
 app.use(router);
+
+
+
+// API routes
+var tvshows = express.Router();
+
+tvshows.route('/tvshows')
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
+
+mongoose.connect('mongodb://localhost/tvshows',function(err,res){
+    if(err){
+        console.log('Error: conecting to datebase. '+err);
+    }
+
+
+});
 
 app.listen(3000, function() {
   console.log("Node server running on http://localhost:3000");
